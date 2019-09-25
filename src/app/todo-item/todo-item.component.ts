31938d93e8a693d04683bbc945439d83fabc580c
Alexandre Demeure <alexandre.demeure@imag.fr>
2019-09-25 19:18:26 +0200

    Update todo-item.component.ts

--- src/app/todo-item/todo-item.component.ts
+++ src/app/todo-item/todo-item.component.ts
@@ -1,6 +1,4 @@
-import {ChangeDetectionStrategy, Component, ElementRef, Input, OnInit, ViewChild} from '@angular/core';
-import {TodoItem} from '../definitions';
-import {TodoService} from '../todo.service';
+import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-todo-item',
@@ -9,37 +7,10 @@ import {TodoService} from '../todo.service';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class TodoItemComponent implements OnInit {
-  @Input() item: TodoItem;
-  @ViewChild('newTextInput', {static: true}) newTextInput: ElementRef<HTMLInputElement>;
-  private pisEditing = false;
 
-  constructor(private tds: TodoService) { }
+  constructor() { }
 
   ngOnInit() {
   }
 
-  get isEditing(): boolean {
-    return this.pisEditing;
-  }
-
-  set isEditing(e: boolean) {
-    this.pisEditing = e;
-    // Donner le focus au champs input
-    requestAnimationFrame( () => this.newTextInput.nativeElement.focus() );
-  }
-
-  remove() {
-    this.tds.remove(this.item);
-  }
-
-  updateIsDone(b: boolean) {
-    this.tds.updateIsDone(b, this.item);
-  }
-
-  updateLabel(label: string) {
-    if (this.isEditing) {
-      this.isEditing = false;
-      this.tds.updateLabel(label, this.item);
-    }
-  }
 }
