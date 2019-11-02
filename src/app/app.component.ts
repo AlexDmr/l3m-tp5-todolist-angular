9f5e0c0d542bc2a40c0e6240386483b6fde1a84d
AlexDmr <alexandre.demeure@univ-grenoble-alpes.fr>
2019-11-02 15:08:54 +0100

    Correction Debug

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import {TodoService} from './todo.service';
 import {TodoItem, TodoList} from './definitions';
 import {Observable} from 'rxjs';
+import { List } from 'immutable';
 
 @Component({
   selector: 'app-root',
@@ -14,6 +15,30 @@ export class AppComponent {
 
   get todoListObs(): Observable<TodoList> {
     return this.tds.observable;
+  }
+
+  append(label: string) {
+    this.tds.append(label);
+  }
+
+  delete(item: TodoItem) {
+    this.tds.remove(item);
+  }
+
+  setDone(item: TodoItem, isDone: boolean) {
+    this.tds.updateIsDone(isDone, item);
+  }
+
+  setLabel(item: TodoItem, label: string) {
+    this.tds.updateLabel(label, item);
+  }
+
+  isAllDone(items: List<TodoItem>): boolean {
+    return items.reduce( (acc, item) => acc && item.isDone, true);
+  }
+
+  setAllDone(done: boolean, items: List<TodoItem>) {
+    this.tds.updateIsDone(done, ...items);
   }
 
 }
