365ef770d07d84d1908c77deb66f15a90af9d086
AlexDmr <alexandre.demeure@univ-grenoble-alpes.fr>
2019-11-14 10:19:50 +0100

    valence 1

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -1,4 +1,7 @@
 import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
+import {TodoService} from '../todo.service';
+import {Observable} from 'rxjs';
+import {TodoList} from '../definitions';
 
 @Component({
   selector: 'app-todo-list',
@@ -8,9 +11,12 @@ import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
 })
 export class TodoListComponent implements OnInit {
 
-  constructor() { }
+  constructor(private tds: TodoService) { }
 
   ngOnInit() {
   }
 
+  get todoListObs(): Observable<TodoList> {
+    return this.tds.observable;
+  }
 }
