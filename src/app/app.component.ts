df54dbc38cd9c2b2ecdb6c496ac8262fb91f7d55
AlexDmr <alexandre.demeure@univ-grenoble-alpes.fr>
2019-11-19 12:48:53 +0100

    mardi 19/11

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,4 +16,34 @@ export class AppComponent {
     return this.tds.observable;
   }
 
+  get toutEstFait(): boolean {
+    return this.tds.getCurrentTodoList().items.find(
+      item => !item.isDone
+    ) === undefined;
+  }
+
+  ToutFaireOuDefaire() {
+    const isDone = !this.toutEstFait;
+    this.tds.updateIsDone(isDone, ...this.tds.getCurrentTodoList().items);
+  }
+
+  append(label: string) {
+    this.tds.append(label);
+  }
+
+  trackByIndex(index: number, ref: TodoItem) {
+    return index;
+  }
+
+  updateItemLabel(item: TodoItem, label: string) {
+    this.tds.updateLabel(label, item);
+  }
+
+  updateItemDone(item: TodoItem, isDone: boolean) {
+    this.tds.updateIsDone(isDone, item);
+  }
+
+  deleteItem(item: TodoItem) {
+    this.tds.remove(item);
+  }
 }
